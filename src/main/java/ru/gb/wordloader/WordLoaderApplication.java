e46bbf9b89d15c0019d6d7bdd9485096e8509ee4
UgryumovAlex <79602945+UgryumovAlex@users.noreply.github.com>
2022-11-18 23:06:23 +0300

    Initial commit

---
+++ src/main/java/ru/gb/wordloader/WordLoaderApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.wordloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordLoaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gb/wordloader/WordLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.wordloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordLoaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
