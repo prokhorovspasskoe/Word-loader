ed0773f3ce12a428d281430054be5941ad6d063f
UgryumovAlex <79602945+UgryumovAlex@users.noreply.github.com>
2022-11-23 23:25:09 +0300

    User : entity, dto, repository. Initial commit

---
+++ src/main/java/ru/gb/wordloader/entities/User.java
@@ -0,0 +1,29 @@
+package ru.gb.wordloader.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="password")
+    private String password;
+
+}
