6a8ec0e50c8a53085d933f54ea36b69c332b1162
Prokhorov <prokhorovspasskoe@yandex.ru>
2022-12-02 19:47:19 +0300

    Метод создания словаря

--- src/main/java/ru/gb/wordloader/converters/UserConverter.java
+++ src/main/java/ru/gb/wordloader/converters/UserConverter.java
@@ -4,7 +4,7 @@ import ru.gb.wordloader.dto.UserDto;
 import ru.gb.wordloader.entities.User;
 
 public class UserConverter {
-    User convert(UserDto userDto){
+   public User convert(UserDto userDto){
         User user = new User();
         user.setName(user.getName());
         user.setPassword(userDto.getPassword());
---
+++ src/main/java/ru/gb/wordloader/repositories/VocabularyRepository.java
@@ -0,0 +1,9 @@
+package ru.gb.wordloader.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.gb.wordloader.entities.Vocabulary;
+
+@Repository
+public interface VocabularyRepository extends JpaRepository<Vocabulary, Long> {
+}
