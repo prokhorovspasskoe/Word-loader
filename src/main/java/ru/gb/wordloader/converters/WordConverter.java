a59342efe85ac26739fbc88327c692e113496940
UgryumovAlex <79602945+UgryumovAlex@users.noreply.github.com>
2022-12-09 21:46:17 +0300

    Переработаны / реализованы WordDto, VocabularyDto, StudyPlanDto, UserDto. Переработаны / реализованы конвертеры к ним.

--- src/main/java/ru/gb/wordloader/converters/WordConverter.java
+++ src/main/java/ru/gb/wordloader/converters/WordConverter.java
@@ -3,10 +3,36 @@ package ru.gb.wordloader.converters;
 import ru.gb.wordloader.dto.WordDto;
 import ru.gb.wordloader.entities.Word;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class WordConverter {
+
+    public static WordDto convertToDto(Word word) {
+        return WordDto.builder()
+                .id(word.getId())
+                .original(word.getOriginal())
+                .translated(word.getTranslated())
+                .build();
+    }
+
+    public static Word convertFromDto(WordDto wordDto) {
+        return Word.builder()
+                .id(wordDto.getId())
+                .original(wordDto.getOriginal())
+                .translated(wordDto.getTranslated())
+                .build();
+    }
+
+    public static List<WordDto> convertToDtoList(List<Word> words) {
+        return words.stream().map(p -> convertToDto(p)).collect(Collectors.toList());
+    }
+
+    public static List<Word> convertFromDtoList(List<WordDto> wordDtos) {
+        return wordDtos.stream().map(p -> convertFromDto(p)).collect(Collectors.toList());
+    }
+
+    /*
     public List<Word> convertFromDtoToEntity(List<WordDto> wordsDto){
         List<Word> words = new ArrayList<>();
         Word bufferWord = new Word();
@@ -35,4 +61,5 @@ public class WordConverter {
 
         return wordDtoList;
     }
+    */
 }
--- src/main/java/ru/gb/wordloader/dto/UserDto.java
+++ src/main/java/ru/gb/wordloader/dto/UserDto.java
@@ -5,13 +5,19 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class UserDto {
     private Long id;
+
     private String username;
-    private String password;
-    private String matchingPassword;
+
+    private List<VocabularyDto> vocabularies;
+
+    private List<StudyPlanDto> studyPlans;
+
 }
